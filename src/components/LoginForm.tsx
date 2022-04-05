e13ccee6362d8aa97bfafe2ef5174b699bc4be80
Jura <mr.gordien@mail.ru>
2022-04-05 04:39:11 +0300

    login-logic
    login-logic

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,36 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Button, Form, Input} from "antd";
 import {rules} from "../utils/rules";
+import {useDispatch} from "react-redux";
+import {AuthActionCreators} from "../store/reducers/auth/action-creators";
+import {useTypedSelector} from "../hooks/useTypedSelector";
 
 const LoginForm: FC = () => {
+    const dispatch = useDispatch()
+    const {error, isLoading} = useTypedSelector(state => state.auth);
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const submit = () => {
+        dispatch(AuthActionCreators.login(username, password))
+    }
+
     return (
-        <Form>
+        <Form
+            onFinish={submit}
+        >
+            {error &&<div style={{color: 'red'}}>
+                {error}
+            </div>}
+
             <Form.Item
                 label="Имя пользователя"
                 name="username"
                 rules={[rules.required("Пожалуйста введите имя пользователя")]}
             >
-                <Input/>
+                <Input
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
             </Form.Item>
 
             <Form.Item
@@ -18,10 +38,14 @@ const LoginForm: FC = () => {
                 name="password"
                 rules={[rules.required("Пожалуйста введите пароль")]}
             >
-                <Input/>
+                <Input
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type={"password"}
+                />
             </Form.Item>
             <Form.Item>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" loading={isLoading}>
                     Войти
                 </Button>
             </Form.Item>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,27 +3,30 @@ import {Layout, Menu, Row} from "antd";
 import {useHistory} from 'react-router-dom'
 import {RoteNames} from "../router";
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {AuthActionCreators} from "../store/reducers/auth/action-creators";
+import {useDispatch} from "react-redux";
 
 const Navbar: FC = () => {
     const router = useHistory()
-    const {isAuth} = useTypedSelector(state => state.auth)
+    const {isAuth, user} = useTypedSelector(state => state.auth)
+    const dispatch = useDispatch()
     return (
         <Layout.Header>
             <Row justify="end">
                 {isAuth
                     ?
                     <>
-                    <div style={{color: 'white'}}>
-                        juracucumber
-                    </div>
-                    <Menu theme="dark" mode="horizontal" selectable={false}>
-                        <Menu.Item
-                            onClick={() => console.log('Выйти')}
-                            key={1}
-                        >
-                            Выйти
-                        </Menu.Item>
-                    </Menu>
+                        <div style={{color: 'white'}}>
+                            {user.username}
+                        </div>
+                        <Menu theme="dark" mode="horizontal" selectable={false}>
+                            <Menu.Item
+                                onClick={() => dispatch(AuthActionCreators.logout())}
+                                key={1}
+                            >
+                                Выйти
+                            </Menu.Item>
+                        </Menu>
                     </>
                     :
                     <Menu theme="dark" mode="horizontal" selectable={false}>
